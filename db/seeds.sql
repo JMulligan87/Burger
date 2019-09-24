72d24b1455029b4cc5a6f32b02731bcefd62bb4d
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-09-24 11:50:57 -0400

    created seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("black and blue burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bacon cheddar", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("pizza burger", false);
