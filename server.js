b0161f6a73a21393515449417bac2d93dfaacaf5
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-09-27 12:21:12 -0400

    almost fully functional

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/views/public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
---
+++ views/public/assets/js/burgers.js
@@ -0,0 +1,40 @@
+$(function() {
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+        var newBurger = {
+            burger_name: $("#burger").val().trim(),
+            devoured: 0
+        }
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger");
+                location.reload();
+            }
+        )
+    });
+
+    $("#eat").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+        var eaten = {
+            devoured: 1
+        };
+
+        $.ajax("/api/burgers" + id, {
+            type: "PUT",
+            data: eaten
+        }).then(
+            function() {
+                console.log("devoured burger");
+                location.reload();
+            }
+        )
+    })
+
+});
+
